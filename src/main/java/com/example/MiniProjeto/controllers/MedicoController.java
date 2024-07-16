b18b1acdf8a1354419a4972f6dc36d38c7c8af0c
GabrielFilomeno <biel_floripa@hotmail.com>
2024-07-16 16:48:59 -0300

    Ex4

--- src/main/java/com/example/MiniProjeto/controllers/MedicoController.java
+++ src/main/java/com/example/MiniProjeto/controllers/MedicoController.java
@@ -2,10 +2,8 @@ package com.example.MiniProjeto.controllers;
 
 import com.example.MiniProjeto.dtos.MedicoRequest;
 import com.example.MiniProjeto.services.MedicoService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.websocket.server.PathParam;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/medico")
@@ -20,5 +18,10 @@ public class MedicoController {
     @PostMapping("/cadastrar")
     public void cadastrarMedico(@RequestBody MedicoRequest medicoRequest){
         medicoService.cadastrarMedico(medicoRequest);
+    }
+
+    @PutMapping("/atualizar/{medicoId}")
+    public void atualizarMedico(@PathVariable Long medicoId, @RequestBody MedicoRequest medicoRequest){
+        medicoService.atualizarMedico(medicoId, medicoRequest);
     }
 }
--- src/main/java/com/example/MiniProjeto/services/MedicoService.java
+++ src/main/java/com/example/MiniProjeto/services/MedicoService.java
@@ -1,8 +1,12 @@
 package com.example.MiniProjeto.services;
 
 import com.example.MiniProjeto.dtos.MedicoRequest;
+import com.example.MiniProjeto.entities.MedicoEntity;
 import com.example.MiniProjeto.repositories.MedicoRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 import static com.example.MiniProjeto.mappers.MedicoMapper.map;
 
@@ -17,5 +21,18 @@ public class MedicoService {
     public void cadastrarMedico(MedicoRequest medicoRequest) {
         //DOTO: tratar erro entity not found e duplicate key
         medicoRepository.save(map(medicoRequest));
+    }
+
+    public void atualizarMedico(Long medicoId, MedicoRequest medicoRequest) {
+        //DOTO: tratar erros
+        MedicoEntity medicoEntity = medicoRepository.findById(medicoId).orElseThrow(EntityNotFoundException::new);
+
+        medicoEntity.setNome(medicoRequest.getNome());
+        medicoEntity.setCrm(medicoRequest.getCrm());
+        medicoEntity.setDataNascimento(medicoRequest.getDataNascimento());
+        medicoEntity.setTelefone(medicoRequest.getTelefone());
+        medicoEntity.setEspecialidade(medicoRequest.getEspecialidade());
+
+        medicoRepository.save(medicoEntity);
     }
 }
